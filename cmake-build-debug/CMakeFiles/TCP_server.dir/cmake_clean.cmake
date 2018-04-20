8a19d500fa4f281c7a7448b66b502ec972d98a05
Victor <victor.kataev@gmail.com>
2018-04-20 13:22:47 +0200

    creation

---
+++ cmake-build-debug/CMakeFiles/TCP_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TCP_server.dir/main.cpp.o"
+  "TCP_server.pdb"
+  "TCP_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TCP_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
